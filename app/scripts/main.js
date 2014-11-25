a8611e98b83571b02fb54a088ede118c5926f64f
Mike Zevitas <mzevitas@gmail.com>
2014-11-25 11:51:19 -0500

    layout work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('The Iron Yard Rocks');
+(function () {
+
+  angular.module('Recipe', ['ngRoute'])
+    .constant('PARSE_HEADERS', {
+      headers: {
+        'X-Parse-Application-Id': 'FYN9rd7vsrQZXkBs1wV24CsZjHqoK8UnYSAtjAFt',
+        'X-Parse-REST-API-Key': 'x0slvTxdLwWvbU3O6qzFU0NuWEhJ5nkcx0khi3EK',
+        'Content-Type': 'application/json'
+      }
+    })
+    .config( function ($routeProvider) {
+    //
+    //  $routeProvider.when('/', {
+    //    templateUrl: 'scripts/whiskey/home.html',
+    //    controller: 'WhiskeyController'
+    //  });
+    //
+    //  $routeProvider.when('/add', {
+    //    templateUrl: 'scripts/whiskey/add.html',
+    //    controller: 'WhiskeyController'
+    //  })
+    //
+    });
+
+}());
