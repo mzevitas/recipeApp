0655837df2f62cca4c75ff75b7d6efe23f525eb3
Mike Zevitas <mzevitas@gmail.com>
2014-11-26 00:57:14 -0500

    end on day 1

--- app/scripts/users/users_login.js
+++ app/scripts/users/users_login.js
@@ -2,12 +2,13 @@
 
   angular.module('Recipe')
     .controller('UserLoginController', ['$scope', '$http', '$location', 'PARSE_HEADERS',
-    function ($scope, $http, $location, PARSE_HEADERS) {
+    function ($scope, $http, $location, PARSE_HEADERS){
 
+    }
 
-
-
-    }]);
-
+      $scope.addWhiskey = function (whiskey) {
+        WhiskeyFactory.addWhiskey(whiskey);
+      };
+    ]);
 
 }());
